5fb2878cfb3f8c2d11455f8e76c0b9c353efbc51
shamalsurange123 <98728126+shamalsurange123@users.noreply.github.com>
2022-01-31 11:01:34 +0530

    Add files via upload

---
+++ CatgoryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using mvc_poduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvc_poduct.Data
+{
+    public class CatgoryDbContext:DbContext
+    {
+        public CatgoryDbContext():base("Om")
+        {
+
+        }
+        public DbSet<Category> category { get; set; }
+        public DbSet<Product> product { get; set; }
+
+    }
+}
