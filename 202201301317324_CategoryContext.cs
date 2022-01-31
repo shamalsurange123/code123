5a669752a9be7ce184cb35b446f115798be31d67
shamalsurange123 <98728126+shamalsurange123@users.noreply.github.com>
2022-01-31 11:03:12 +0530

    Add files via upload

---
+++ 202201301317324_CategoryContext.cs
@@ -0,0 +1,42 @@
+﻿namespace mvc_poduct.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CategoryContext : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryID = c.Int(nullable: false, identity: true),
+                        Category_Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.CategoryID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Product_Id = c.Int(nullable: false, identity: true),
+                        Product_Name = c.String(nullable: false),
+                        Product_Price = c.Double(nullable: false),
+                        CategoryID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Product_Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryID, cascadeDelete: true)
+                .Index(t => t.CategoryID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "CategoryID", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "CategoryID" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace mvc_poduct.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<mvc_poduct.Data.CatgoryDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(mvc_poduct.Data.CatgoryDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
