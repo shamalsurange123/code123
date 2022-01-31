04a53ef41181b26100fa829341715edc5de46d69
shamalsurange123 <98728126+shamalsurange123@users.noreply.github.com>
2022-01-31 10:59:42 +0530

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,95 @@
+﻿using mvc_poduct.Generic_Repos;
+using mvc_poduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_poduct.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductRepos productRepos = new ProductRepos();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(productRepos.GetAll());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+          
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                productRepos.AddEntity(product);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
