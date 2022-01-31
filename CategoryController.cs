8c4bd79011c44828618988df0c6d3c5a9f757c54
shamalsurange123 <98728126+shamalsurange123@users.noreply.github.com>
2022-01-31 11:01:03 +0530

    Add files via upload

---
+++ CategoryController.cs
@@ -0,0 +1,93 @@
+﻿using mvc_poduct.Generic_Repos;
+using mvc_poduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_poduct.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryRepos categoryRepos = new CategoryRepos();
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View(categoryRepos.GetAll());
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                categoryRepos.AddEntity(category);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
