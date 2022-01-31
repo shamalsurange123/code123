6513006abe0405e7995ad20c54ed3f681c0019ab
shamalsurange123 <98728126+shamalsurange123@users.noreply.github.com>
2022-01-31 11:03:53 +0530

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace mvc_poduct.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int CategoryID { get; set; }
+        [Required]
+        public string Category_Name { get; set; }
+
+
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace mvc_poduct.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        public int Product_Id { get; set; }
+        [Required]
+        public string Product_Name { get; set; }
+        [Required]
+        public double Product_Price { get; set; }
+
+        public int CategoryID { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+    }
+}
